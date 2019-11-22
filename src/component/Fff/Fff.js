06a528c1c63b0015c442e41102e1fc56fbfd6f93
freya0608 <freya001@yeah.net>
2019-11-22 17:22:02 +0800

    init

--- src/component/Fff/Fff.js
+++ src/component/Fff/Fff.js
@@ -1,58 +1,32 @@
-/**
- * Created by ${USER} on ${DATE}.
- * https://www.jetbrains.com/help/webstorm/file-template-variables.html
- 动画callback只支持1.x版本的TransitionGroup
- */
-import React,{Component} from 'react';
-import './fff.css';
-const styles = {
-    container: {}
-};
-//import ReactDOM from 'react-dom';
-//import {TweenMax} from "gsap";
-//import PropTypes from 'prop-types';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactFullpage from '@fullpage/react-fullpage';
 
-class Fff extends React.Component {
-    static defaultProps = {
-        ...Component.defaultProps
-    }
-    static propTypes = {}
-    constructor(props){
-        super(props)
-        this.state = {}
-        this.dom=React.createRef()
-        //React.createRef();current
-        //事件绑定在es6中用于自定义事件props事件不适用
-        //this.handleClick = this.handleClick.bind(this);
-    }
-    //组件将要装载
-    //componentWillMount(){}
-    //组件加载完毕
-    componentDidMount(){
-        //this.dom.root=ReactDOM.findDOMNode(this);
-    }
-    //组件将接收道具
-    //componentWillReceiveProps(nextProps){}
-    //shouldComponentUpdate(nextProps, nextState) {}
-    //组件将更新
-    //componentWillUpdate(nextProps, nextState){}
-    //组件更新完毕
-    //componentDidUpdate(nextProps, nextState){}
-    //组件将要卸载
-    //componentWillUnmount(){}
+const Fullpage = () => (
+    <ReactFullpage
+        //fullpage options
+        licenseKey = {'YOUR_KEY_HERE'}
+        scrollingSpeed = {1000} /* Options here */
 
-    /*动画*/
-    //componentWillAppear(callback){}
-    //componentDidAppear(){}
-    //componentWillEnter(callback){}
-    //componentDidEnter(){}
-    //componentWillLeave(callback){}
-    //componentDidLeave(){}
-    render() {
-        return (
-            <div ref={this.dom}>fffff</div>
-        );
-    }
-}
+        render={({ state, fullpageApi }) => {
+            return (
+                <ReactFullpage.Wrapper>
+                    <div className="section">
+                        <p>Section 1 (welcome to fullpage.js)</p>
+                        <button onClick={() => fullpageApi.moveSectionDown()}>
+                            Click me to move down
+                        </button>
+                    </div>
+                    <div className="section">
+                        <p>Section 2</p>
+                    </div>
+                    <div className="section">
+                        <p>Section 3</p>
+                    </div>
+                </ReactFullpage.Wrapper>
+            );
+        }}
+    />
+);
 
-export default Fff;
+export default Fullpage
